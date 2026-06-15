a1ee18067fad0ef64cdd776d3bddfe9dd7528d9b
janesh008 <36275043+janesh008@users.noreply.github.com>
2026-06-15 13:39:11 +0530

    fixed error

--- api/index.js
+++ api/index.js
@@ -5,5 +5,17 @@ export const config = {
 import handler from "../dist/server/server.js";
 
 export default function (request) {
-  return handler.fetch(request, process.env, {});
+  const host = request.headers.get("host");
+  const proto = request.headers.get("x-forwarded-proto") || "https";
+  const url = new URL(request.url, `${proto}://${host}`);
+
+  const hasBody = request.method !== "GET" && request.method !== "HEAD";
+  const absoluteRequest = new Request(url, {
+    method: request.method,
+    headers: request.headers,
+    body: hasBody ? request.body : undefined,
+    duplex: hasBody ? "half" : undefined,
+  });
+
+  return handler.fetch(absoluteRequest, process.env, {});
 }
